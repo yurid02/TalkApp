452a6a35e7577b09cf1a61eb39b558926d9e5bb5
pc <raoswafa93@gmail.com>
2022-05-19 18:21:21 +0300

    The Final Project - Talk App

---
+++ app/src/main/java/com/example/talkapp/LoginFr.java
@@ -0,0 +1,31 @@
+package com.example.talkapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginFr extends Fragment {
+
+    public LoginFr() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_log_in, container, false);
+    }
+}
